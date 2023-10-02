5c934c78a724b53f383b39eeec5a10dc4879f8d7
Yuki-Yui <yuukiyuinyan@gmail.com>
2023-10-02 20:34:23 +0900

    読み込みが遅れるのを修正

--- change_select.js
+++ change_select.js
@@ -1,5 +1,18 @@
 let gakkicode = 0;
 
+function load_storage() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get("gakki", (data) => {
+            if (chrome.runtime.lastError) {
+                reject(chrome.runtime.lastError);
+            } else {
+                gakkicode = data.gakki;
+                resolve(data);
+            }
+        });
+    });
+}
+
 chrome.storage.local.get("gakki", function(data) {
     gakkicode = data.gakki;
 });
@@ -13,14 +26,18 @@ function change(){
     }    
 }
 
-
-window.addEventListener('load', function() {
-    const gakki = document.getElementById('gakkiKubunCode');
-    if(gakki != null){
-        gakki.value = gakkicode;
-    }
-    else if(frameDOM != null) {
-        frameDOM.onload = () => change();
+window.addEventListener('load', async function() {
+    try {
+        await load_storage();
+        const gakki = document.getElementById('gakkiKubunCode');
+        if(gakki != null){
+            gakki.value = gakkicode;
+        }
+        else if(frameDOM != null) {
+            frameDOM.onload = () => change();
+        }
+    } catch (error) {
+        return;
     }
 });
     
