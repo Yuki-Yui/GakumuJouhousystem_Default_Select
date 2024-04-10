34cffa362d16fef8cb153290fb5bcc445a23d863
Yuki-Yui <118826058+Yuki-Yui@users.noreply.github.com>
2024-04-10 13:02:34 +0900

    シラバス検索開始ボタンを自動クリックできるようにした

--- change_select.js
+++ change_select.js
@@ -1,28 +1,31 @@
 let gakkicode = 0;
+let autoClick = false;
 
 function load_storage() {
     return new Promise((resolve, reject) => {
-        chrome.storage.local.get("gakki", (data) => {
+        chrome.storage.local.get(["gakki","autoClick"], (data) => {
             if (chrome.runtime.lastError) {
                 reject(chrome.runtime.lastError);
             } else {
                 gakkicode = data.gakki;
+                autoClick = data.autoClick;
                 resolve(data);
             }
         });
     });
 }
 
-chrome.storage.local.get("gakki", function(data) {
-    gakkicode = data.gakki;
-});
+// chrome.storage.local.get("gakki", function(data) {
+//     gakkicode = data.gakki;
+// });
 
 function change(){
     const frame = document.getElementById("idForChangeMenu").getElementsByTagName("frame")[1].contentWindow;
     const gakki = frame.document.getElementById('gakkiKubunCode');
     if (gakki != null){
         gakki.value = gakkicode;
-    }    
+    }
+
 }
 
 window.addEventListener('load', async function() {
@@ -31,13 +34,40 @@ window.addEventListener('load', async function() {
         const gakki = document.getElementById('gakkiKubunCode');
         if(gakki != null){
             gakki.value = gakkicode;
+            if(!autoClick) return;
+            let allInput = document.querySelectorAll('input');
+            allInput.forEach((input)=>{
+                console.log(input);
+                if (input.value === " 検索開始 ") {
+                    input.click();
+                }
+            });
         }
         else if(frameDOM != null) {
             frameDOM.onload = () => change();
         }
+        frameDOM.onload = () => getImagesByAlt('Face Photo');
     } catch (error) {
         return;
     }
 });
     
 const frameDOM = document.body.getElementsByTagName("frame")[2];
+
+function getImagesByAlt(altValue) {
+    // すべてのimg要素を取得
+    const frame = document.getElementById("idForChangeMenu").getElementsByTagName("frame")[1].contentWindow;
+    let allImages = frame.document.querySelectorAll('img');
+    let imagesWithAlt = [];
+
+    // 画像要素のalt属性をチェックし、特定の値を持つものを抽出
+    allImages.forEach(function(img) {
+        imagesWithAlt.push(img)
+        if (img.alt === altValue) {
+            img.style.height = "auto";
+        }
+    });
+    // console.log(imagesWithAlt);
+    return imagesWithAlt;
+}
+
--- save.js
+++ save.js
@@ -1,17 +1,23 @@
 const gakki = document.getElementById('gakki');
+const autoclick = document.getElementById('autoclick');
 const submitButton = document.getElementById('submitButton');
 const messageBox = document.getElementById('message');
 
-chrome.storage.local.get("gakki", function(data) {
+chrome.storage.local.get(["gakki","autoClick"], (data) => {
     gakkicode = data.gakki;
+    autoClick = data.autoClick;
+    // alert(autoclick);
     gakki.value = gakkicode;
+    autoclick.checked = autoClick;
 });
 
-submitButton.addEventListener('click', function() {
+submitButton.addEventListener('click', () => {
     const gakki_value = gakki.value;
+    const autoclick_value = autoclick.checked;
 
     chrome.storage.local.set({
-        gakki: gakki_value
+        "gakki" : gakki_value,
+        "autoClick" : autoclick_value
     }, function() {
         messageBox.innerHTML = '値を保存しました。<br>ページを再読み込みしてください。';
     });
